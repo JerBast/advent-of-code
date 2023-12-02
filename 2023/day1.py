f2a5205fb2723dc9cea3d25f001c74f661b822f7
Jeroen <jbastenhof@live.nl>
2023-12-02 13:22:09 +0100

    Simplify day 1

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,32 +1,25 @@
 import re
 
-from aocd import get_data
 from typing import List
+from aocd import get_data
 
 DATA: List[str] = get_data(year=2023, day=1).splitlines()
 REPR: List[str] = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 
-def part1():
-    pattern = re.compile('\d')
+def from_repr(s: str):
+    return s if s.isdigit() else str(REPR.index(s))
+
+def solve(regex: str):
+    pattern = re.compile(regex)
 
     tot = 0
     for line in DATA:
-        digits = pattern.findall(line)
-        tot += int(digits[0] + digits[-1])
-    print(tot)
-
-
-def part2():
-    pattern = re.compile(f'(?=(\d|{"|".join(REPR[1:])}))')
-
-    tot = 0
-    for line in DATA:
-        digits = [i if i.isdigit() else str(REPR.index(i)) for i in pattern.findall(line)]
+        digits = [from_repr(i) for i in pattern.findall(line)]
         tot += int(digits[0] + digits[-1])
     print(tot)
 
 
 if __name__ == '__main__':
-    part1()
-    part2()
+    solve('\d')
+    solve(f'(?=(\d|{"|".join(REPR[1:])}))')
